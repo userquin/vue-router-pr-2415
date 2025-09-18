7b87475f4959e0da3094bcd48cda58863b3ffa5f
userquin <userquin@gmail.com>
2025-09-18 18:59:44 +0200

    chore: cleanup vite config file

--- vite-app/vite.config.ts
+++ vite-app/vite.config.ts
@@ -1,15 +1,9 @@
-import { fileURLToPath } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import router from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
   plugins: [
     router({
       // _inspect: true,
